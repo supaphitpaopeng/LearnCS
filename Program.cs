490adb2c8e501542f9e0d58e246d422912d14cfa
supaphitpaopeng <supaphit.pa.67@ubu.ac.th>
2026-06-17 10:08:19 +0700

    add int variables

--- Program.cs
+++ Program.cs
@@ -11,6 +11,10 @@ namespace LearnCS
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            int a = 5 , b = 2;  // comment 1 line
+            /* comment
+             2 line */
+            Console.WriteLine($"a + b = {a + b}");
         }
     }
 }
