cc218140ad9a81274cdfea11fd3f2bd083116a4c
supaphitpaopeng <supaphit.pa.67@ubu.ac.th>
2026-06-17 10:20:05 +0700

    add more cars brands

--- Program.cs
+++ Program.cs
@@ -15,6 +15,13 @@ namespace LearnCS
             /* comment
              2 line */
             Console.WriteLine($"a + b = {a + b}");
+            string[] cars = new string[3];
+            cars[0] = "Volvo";
+            cars[1] = "BMW";
+            cars[2] = "Ford";
+            Console.WriteLine($"First car: {cars[0]}");
+            Console.WriteLine($"Second car: {cars[1]}");
+            Console.WriteLine($"Third car: {cars[2]}");
         }
     }
 }
